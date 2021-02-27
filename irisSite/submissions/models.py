76780e9dc624263b65dc37a873d77946bd71d706
Henry <tronlover101@gmail.com>
2021-02-26 18:38:22 -0600

    added form and stuff idk but it works

--- irisSite/submissions/models.py
+++ irisSite/submissions/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 
 class Submission(models.Model):
     submission_text = models.TextField(max_length=300)
     reported = models.BooleanField(default=False)
+
+class SubmissionForm(ModelForm):
+    class Meta:
+        model = Submission
+        fields = ['submission_text']
--- irisSite/submissions/views.py
+++ irisSite/submissions/views.py
@@ -1,5 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from better_profanity import profanity
+from django.views import generic
 
+from .models import Submission, SubmissionForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the submissions index.")
+    if request.method == 'POST':
+        submission = SubmissionForm(request.POST)
+        if submission.is_valid():
+            submission = SubmissionForm(request.POST)
+            new_submission = submission.save(commit=False)
+            submission_text = new_submission.submission_text
+            output = profanity.censor(submission_text)
+            if submission_text != output:
+                new_submission.reported = True
+            else:
+                new_submission.reported = False
+            new_submission.save()
+            return redirect('index')
+    else:
+        submission = SubmissionForm()
+    return render(request, 'submissions/index.html', {'form': submission})
+        
+def submitpost(request):
+    submission = SubmissionForm(request.POST)
+    new_submission = submission.save(commit=False)
+    submission_text = new_submission.submission_text
+    output = profanity.censor(submission_text)
+    if submission_text != output:
+        new_submission.reported = True
+    else:
+        new_submission.reported = False
+    new_submission.save()
+
